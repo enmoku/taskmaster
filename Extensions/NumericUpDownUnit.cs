0bed18fd0464c0a1b8e9eeaf92592e5def3a2241
MKA <8120158+mkahvi@users.noreply.github.com>
2018-11-16 19:11:19 +0200

    For showing unit in the scroller

---
+++ Extensions/NumericUpDownUnit.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Taskmaster.Extensions
+{
+	/// <summary>
+	/// Extension on NumericUpDown to show some form of unit of measure.
+	/// </summary>
+	class NumericUpDownEx : NumericUpDown
+	{
+		public NumericUpDownEx() : base()
+		{
+			// nothing here
+		}
+
+		public string Unit { get; set; } = null;
+
+		protected override void UpdateEditText()
+		{
+			if (!string.IsNullOrEmpty(Unit)) Text = $"{Value} {Unit}";
+			else base.UpdateEditText();
+		}
+	}
+}
