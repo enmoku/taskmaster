1010e00943d7bed9d22dd80f2efefa21f22c27b3
MKA <8120158+mkahvi@users.noreply.github.com>
2019-01-10 19:17:04 +0200

    Improve docs

--- NativeMethods.cs
+++ NativeMethods.cs
@@ -131,10 +131,25 @@ namespace Taskmaster
 		[Flags]
 		public enum SendMessageTimeoutFlags : uint
 		{
+			/// <summary>
+			/// The calling thread is not prevented from processing other requests while waiting for the function to return.
+			/// </summary>
 			SMTO_NORMAL = 0x0,
+			/// <summary>
+			/// Prevents the calling thread from processing any other requests until the function returns.
+			/// </summary>
 			SMTO_BLOCK = 0x1,
+			/// <summary>
+			/// The function returns without waiting for the time-out period to elapse if the receiving thread appears to not respond or "hangs."
+			/// </summary>
 			SMTO_ABORTIFHUNG = 0x2,
+			/// <summary>
+			/// The function does not enforce the time-out period as long as the receiving thread is processing messages.
+			/// </summary>
 			SMTO_NOTIMEOUTIFNOTHUNG = 0x8,
+			/// <summary>
+			/// The function should return 0 if the receiving window is destroyed or its owning thread dies while the message is being processed.
+			/// </summary>
 			SMTO_ERRORONEXIT = 0x20
 		}
 
