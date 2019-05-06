cd4e58e8922651f10680f9c77be93d2fdb6b12a5
MKA <8120158+mkahvi@users.noreply.github.com>
2019-05-06 22:43:28 +0300

    Missed?

--- Audio/MicManager.cs
+++ Audio/MicManager.cs
@@ -28,6 +28,7 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Threading.Tasks;
 using MKAh;
 using MKAh.Ini;
 using NAudio.CoreAudioApi;
