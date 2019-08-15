327ecc6aa154d774a5865effe564cb5fb2ab97aa
MKA <8120158+mkahvi@users.noreply.github.com>
2019-08-15 16:57:13 +0300

    Deterministic is annoying

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Runtime.InteropServices;
 
 [assembly: ComVisible(false)]
 
-[assembly: AssemblyVersion("0.14.0.0")] // FIXME: Get this somehow from the solution settings?
-[assembly: AssemblyFileVersion("0.14.0.0")]
+[assembly: AssemblyVersion("0.14.*")] // FIXME: Get this somehow from the solution settings?
+[assembly: AssemblyFileVersion("0.14.*")]
 [assembly: System.Resources.NeutralResourcesLanguage("en")]
 [assembly: Guid("088f7210-51b2-4e06-9bd4-93c27a973874")] // this is pointless, yes?
