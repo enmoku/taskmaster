1996fd487af16cc2ea3122e57f7912b5114e748f
MKA <8120158+mkahvi@users.noreply.github.com>
2019-09-04 15:53:11 +0300

    TimeSpan.IsZero() extension

---
+++ Utility/TimeSpanExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taskmaster
+{
+	public static partial class TimeSpanExtensions
+	{
+		public static bool IsZero(this TimeSpan value) => value == TimeSpan.Zero;
+	}
+}
